cf3f8276cf2fe82a3949b23b195ffb08c95a2dda
Wellbek <louis.wellmeyer@gmail.com>
2024-10-26 14:44:35 +0200

    Add unix bash configuration

--- config.lua
+++ config.lua
@@ -19,14 +19,26 @@ lvim.transparent_window = true
 -- Leader key configuration
 lvim.leader = "space"
 
--- PowerShell Configuration
-vim.opt.shell = "pwsh.exe"
-vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
-vim.cmd [[
-    let &shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-    let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-    set shellquote= shellxquote=
-]]
+-- Check the operating system
+if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
+    -- PowerShell Configuration for Windows
+    vim.opt.shell = "pwsh.exe"
+    vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+    vim.cmd [[
+        let &shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+        let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+        set shellquote= shellxquote=
+    ]]
+else
+    -- Bash Configuration for Linux and Unix-like systems
+    vim.opt.shell = "bash"
+    vim.opt.shellcmdflag = "-c"
+    vim.cmd [[
+        let &shellredir = '2>&1 | tee %s | exit $LastExitCode'
+        let &shellpipe = '2>&1 | tee %s | exit $LastExitCode'
+        set shellquote= shellxquote=
+    ]]
+end
 
 -- Clipboard Configuration
 vim.g.clipboard = {
